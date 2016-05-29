99db71dd841526b3d2b4738dd9702125df80ea58
Spencer Sharp <spencersharp@me.com>
2016-05-29 16:12:33 -0500

    DynamoDB Versioning added

---
+++ APTestPracticeApp/app/src/main/java/com/example/spencersharp/aptestpracticeapp/Version.java
@@ -0,0 +1,38 @@
+package com.example.spencersharp.aptestpracticeapp;
+
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBHashKey;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBIgnore;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBTable;
+
+@DynamoDBTable(tableName = "Version")
+public class Version
+{
+    long version;
+
+    public Version()
+    {
+
+    }
+
+    public Version(long v)
+    {
+        version = v;
+    }
+
+    @DynamoDBHashKey(attributeName = "version")
+    public long getVersion()
+    {
+        return version;
+    }
+
+    public void setVersion(long v)
+    {
+        version = v;
+    }
+
+    @DynamoDBIgnore
+    public void increment()
+    {
+        version++;
+    }
+}
